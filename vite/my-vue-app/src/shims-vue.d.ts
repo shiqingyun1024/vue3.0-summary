816cafb4eb0d35efca47675ec06d727e22c12277
814657780 <18037818812@163.com>
2021-05-28 23:13:37 +0800

    增加.d.ts的类型声明文件和在vite中设置alias

--- vite/my-vue-app/src/shims-vue.d.ts
+++ vite/my-vue-app/src/shims-vue.d.ts
@@ -2,3 +2,9 @@ declare module "*.css" {
     const classes:{[key:string]:string};
     export default classes;
 }
+
+declare module "*.vue" {
+    import { defineComponent,FunctionalComponent} from "vue";
+    const component:ReturnType<typeof defineComponent> | FunctionalComponent
+    export default component
+}
