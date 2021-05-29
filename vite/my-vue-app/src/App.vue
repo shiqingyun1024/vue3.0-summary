09e65b0945bd75a0d368ca318c4f2de17099a9a8
814657780 <18037818812@163.com>
2021-05-29 23:23:34 +0800

    获取html中的dom元素的方法,刚开始定义ref(null)

--- vite/my-vue-app/src/App.vue
+++ vite/my-vue-app/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <img alt="Vue logo" src="./assets/vite.jpeg" />
   <!-- <HelloWorld2 msg="Hello Vue 3 + Vite" /> -->
-  <HelloWorld3 />
+  <!-- <HelloWorld3 /> -->
+  <HelloWorld4 />
 </template>
 
 <script setup lang="ts">
-import HelloWorld3 from './components/HelloWorld3.vue'
+// import HelloWorld3 from './components/HelloWorld3.vue'
+import HelloWorld4 from './components/HelloWorld4.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ vite/my-vue-app/src/components/HelloWorld4.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="text" ref="root" value="ref使用例子">
+  <input type="tel" ref="root2" value="手机号码">
+</template>
+
+<script setup lang="ts">
+// templateRefs的使用
+import { ref, onMounted } from "vue"
+
+// 这样就可以拿到ref的值了
+const root = ref(null) // 声明的这个值要和html中ref中的值保持一致
+const root2 = ref(null)// 声明的这个值要和html中ref中的值保持一致
+// DOM首次渲染完成之后 才能获取到真实的DOM
+onMounted(()=>{
+  console.log("ref",root.value); // ref <input type="text">
+  console.log("ref2",root2.value);// ref2 <input type="tel">
+  
+})
+
+</script>
+
+<style>
+
+</style>
