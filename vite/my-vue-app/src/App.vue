0ec549d50478d0ee358ae066d161785f50fc806e
814657780 <18037818812@163.com>
2021-05-29 22:11:57 +0800

    demo案例

--- vite/my-vue-app/src/App.vue
+++ vite/my-vue-app/src/App.vue
@@ -1,10 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img alt="Vue logo" src="./assets/vite.jpeg" />
+  <!-- <HelloWorld2 msg="Hello Vue 3 + Vite" /> -->
+  <HelloWorld2 />
 </template>
 
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld2 from './components/HelloWorld2.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
