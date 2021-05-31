38c14941b583158edc3bce16565fabce9dc60abb
814657780 <18037818812@163.com>
2021-05-31 22:12:37 +0800

    computed

--- vite/my-vue-app/src/App.vue
+++ vite/my-vue-app/src/App.vue
@@ -2,12 +2,13 @@
   <img alt="Vue logo" src="./assets/vite.jpeg" />
   <!-- <HelloWorld2 msg="Hello Vue 3 + Vite" /> -->
   <!-- <HelloWorld3 /> -->
-  <HelloWorld4 />
+  <HelloWorld5 />
 </template>
 
 <script setup lang="ts">
 // import HelloWorld3 from './components/HelloWorld3.vue'
-import HelloWorld4 from './components/HelloWorld4.vue'
+// import HelloWorld4 from './components/HelloWorld4.vue'
+import HelloWorld5 from './components/HelloWorld5.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ vite/my-vue-app/src/components/HelloWorld5.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{plusOne}}</div>
+  <div>{{count}}</div>
+</template>
+
+<script setup lang="ts">
+import {computed,ref} from "vue"
+const count = ref(1)
+// const plusOne = computed(()=>count.value + 1)
+const plusOne = computed({
+  get:()=>count.value+1,
+  set:(val)=>{
+    count.value = val - 1;
+  }
+})
+console.log(plusOne);
+
+</script>
+
+<style>
+
+</style>
