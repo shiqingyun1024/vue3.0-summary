b6cde9597738b1cd62ad482abd8846ea0b682f7d
814657780 <18037818812@163.com>
2021-06-01 22:46:49 +0800

    生命周期

---
+++ vite/my-vue-app/src/components/生命周期.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>{{num}}</div>
+</template>
+
+<script setup lang="ts">
+// 生命周期
+// 开发Hooks
+/**  2.x与3.x对比
+ *   beforeCreate -> use setup()
+ *   created -> use setup()
+ *   beforeMount -> onBeforeMount
+ *   mounted -> onMounted
+ *   beforeUpdate -> onBeforeUpdate
+ *   updated -> onUpdated
+ *   beforeDestory -> onBeforeUnmount
+ *   destroyed -> onUnmounted
+ *   errorCaptured -> onErrorCaptured
+ * */ 
+import {ref,onBeforeMount,onMounted,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted,onErrorCaptured} from "vue";
+const num = ref(0);
+onBeforeMount(()=>{
+  console.log("onBeforeMount");
+})
+onMounted(()=>{
+  console.log("onMounted");
+})
+onBeforeUpdate(()=>{
+  console.log("onBeforeUpdate");
+})
+onUpdated(()=>{
+  console.log("onUpdated");
+})
+onBeforeUnmount(()=>{
+  console.log("onBeforeUnmount");
+})
+onUnmounted(()=>{
+  console.log("onUnmounted");
+})
+onErrorCaptured(()=>{
+  console.log("onErrorCaptured");
+})
+</script>
+
+<style>
+
+</style>
