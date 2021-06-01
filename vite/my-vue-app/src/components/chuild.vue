57baa89cb3a0e208fb8fbaedda3ab8bd97b3a0cd
814657780 <18037818812@163.com>
2021-06-01 22:59:02 +0800

    生命周期

---
+++ vite/my-vue-app/src/components/chuild.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{state.count}}</div>
+</template>
+
+<script lang="ts">
+// watch
+import { defineComponent,inject,Ref,watchEffect} from "vue"
+export default defineComponent({
+  name:"child",
+  setup(){
+    const theme = inject("test","努力")
+  }
+})
+
+
+</script>
+
+<style>
+
+</style>
---
+++ vite/my-vue-app/src/components/provide&injct&expose.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{state.count}}</div>
+</template>
+
+<script setup lang="ts">
+// watch
+import {watch,reactive} from "vue"
+const state = reactive({count:0})
+watch(()=>state.count,(count,prevCount)=>{
+   console.log("第一个监听的值",count,prevCount);
+},{
+  deep:true,
+  immediate:true
+})
+setTimeout(()=>{
+  state.count++
+},1000)
+
+</script>
+
+<style>
+
+</style>
