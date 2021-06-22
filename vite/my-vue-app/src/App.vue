6a1b7ff4440f9b572c1772e0b7c5833dc3ed648d
814657780 <18037818812@163.com>
2021-06-23 00:20:09 +0800

    vue3.0中mock数据和axios挂载到全局

--- vite/my-vue-app/src/App.vue
+++ vite/my-vue-app/src/App.vue
@@ -11,6 +11,23 @@
 // import HelloWorld4 from './components/HelloWorld4.vue'
 // import HelloWorld6 from './components/HelloWorld6.vue'
 import HelloWorld7 from './components/HelloWorld7.vue'
+// import axios from 'axios'
+import { onMounted, getCurrentInstance } from 'vue'
+
+// mock数据
+onMounted(()=>{
+  const {proxy} = getCurrentInstance(); // proxy相当于this
+  const baseUrl = 'https://www.fastmock.site/mock/3108d616ba9a1a9019a6d5b7905a11ea/api'
+  // proxy.$http.get(`${baseUrl}/test`).then(({data})=>{
+  proxy.axios.get(`${baseUrl}/test`).then(({data})=>{
+    console.log(data);
+    console.log(typeof data.code);
+    
+     if(data.code === 0){
+       console.log(data.data);
+     }
+  })
+})
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
--- vite/my-vue-app/src/main.ts
+++ vite/my-vue-app/src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+// 把axios挂载到全局上 --- 方法1
+// app.config.globalProperties.$http = axios;
+
+// 把axios挂载到全局上 --- 方法2
+app.use(VueAxios, axios)
+
+app.mount('#app')
