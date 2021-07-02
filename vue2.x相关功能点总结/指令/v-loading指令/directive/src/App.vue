a7fb01aa5107513928d0de38eaa247427f16ee5a
814657780 <18037818812@163.com>
2021-07-02 22:12:58 +0800

    自定义v-loading指令

--- vue2.x相关功能点总结/指令/v-loading指令/directive/src/App.vue
+++ vue2.x相关功能点总结/指令/v-loading指令/directive/src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <list :datalist="datalist"></list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import list from './components/list'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    list
+  },
+  data(){
+    return {
+      datalist:[
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+        '有的情况下，你仍然需要对普通 DOM 元素进行底层操作，这时候就会用到自定义指令',
+      ]
+    }
+  },
+  methods:{
+
   }
 }
 </script>
---
+++ vue2.x相关功能点总结/指令/v-loading指令/directive/src/components/list.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="list" v-loading>
+    <ul v-for="item in datalist">
+      <li>{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "list",
+  props: {
+    datalist: {
+      type: Array,
+      default: []
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
