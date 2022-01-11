b006e316305ab5b72575094aff19e71305f53dbd
814657780 <18037818812@163.com>
2022-01-11 22:40:45 +0800

    vue3中的computed的用法

--- vue3.0初识/vue3.0-exercise/src/views/About.vue
+++ vue3.0初识/vue3.0-exercise/src/views/About.vue
@@ -3,3 +3,17 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script lang="ts" setup>
+import { ref,computed } from 'vue'
+
+const counter = ref(0)
+const twiceTheCounter = computed(() => counter.value*2)
+counter.value++
+console.log(counter.value);
+console.log(twiceTheCounter.value); // 注意computed返回的值要用value
+
+</script>
+
+<style scoped lang="scss">
+</style>
+
