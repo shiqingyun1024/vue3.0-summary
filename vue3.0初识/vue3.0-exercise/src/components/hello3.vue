02d4ba9d9a94033ce685cba3c3affb69f2bc73be
814657780 <18037818812@163.com>
2022-01-18 21:47:58 +0800

    模板引用

---
+++ vue3.0初识/vue3.0-exercise/src/components/hello3.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <div v-for="(item, i) in list" :key="i" :ref="el => { if (el) divs[i] = el }">
+    {{ item }}
+  </div>
+</div>
+</template>
+
+<script>
+  import { ref, reactive, onBeforeUpdate, onMounted } from 'vue'
+
+  export default {
+    setup() {
+      const list = reactive([1, 2, 3])
+      const divs = ref([])
+
+      // 确保在每次更新之前重置ref
+      onBeforeUpdate(() => {
+        divs.value = []
+      })
+
+      onMounted(()=>{
+        console.log(divs)
+      })
+
+      return {
+        list,
+        divs
+      }
+    }
+  }
+</script>
--- vue3.0初识/vue3.0-exercise/src/views/Home.vue
+++ vue3.0初识/vue3.0-exercise/src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <Hello collectionName="Welcome to Your Vue.js + TypeScript App"/>
     <hello2></hello2>
+    <hello3></hello3>
   </div>
 </template>
 
@@ -10,11 +11,13 @@
 import { Options, Vue } from 'vue-class-component';
 import Hello from '@/components/hello1.vue'; // @ is an alias to /src
 import hello2 from '@/components/hello2.vue'; // @ is an alias to /src
+import hello3 from '@/components/hello3.vue'; // @ is an alias to /src
 
 @Options({
   components: {
     Hello,
-    hello2
+    hello2,
+    hello3
   },
 })
 export default class Home extends Vue {}
