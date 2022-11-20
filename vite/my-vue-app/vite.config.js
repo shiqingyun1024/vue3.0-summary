87c527e068de0b201c66b837844c9126060d29cc
814657780 <18037818812@163.com>
2022-11-20 21:14:31 +0800

    process.env.npm_config_argv的打印

--- vite/my-vue-app/vite.config.js
+++ vite/my-vue-app/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path'
 
+console.log("--------进程--------")
+console.log(process.env)
+console.log("--------进程npm_config_argv--------")
+console.log(process.env.npm_config_argv)
+console.log("--------进程argv--------")
+console.log(process.argv);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
